49bf0097cb5fa60afd7e904c4ee30c607e3ee5ca
mudasserasool <mudasserasool@gmail.com>
2023-04-06 15:40:46 +0500

    sik-aprail-two

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-
 import Form from "./components/form/Form";
 import Table from './components/table/Table'
 import SmsLog from './components/smslog/SmsLog.jsx'
-// import Twenty from "./Twenty";
 
 import { Router, Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
-// import Example from "./components/smslog/Example";
-
 
 const App = () => {
   return (
     <div>
+    
       {/* <Example/> */}
       <Routes>
         <Route path="/" element={<Form />} />
