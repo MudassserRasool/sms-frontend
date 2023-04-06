792de09b542ec2d4098ebefbd7ec37793c58a514
mudasserasool <mudasserasool@gmail.com>
2023-04-06 13:08:23 +0500

    'six-aprail'

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 import Form from "./components/form/Form";
 import Table from './components/table/Table'
-// import Table from "./Table";
-import Twenty from "./Twenty";
+import SmsLog from './components/smslog/SmsLog.jsx'
+// import Twenty from "./Twenty";
 
 import { Router, Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
-import OpenField from "./components/table/OpenField";
-import TriggerRendererProp from "./TriggerRendererProp";
+// import Example from "./components/smslog/Example";
+
 
 const App = () => {
   return (
     <div>
+      {/* <Example/> */}
       <Routes>
         <Route path="/" element={<Form />} />
         <Route path="/table" element={<Table />} />
-        {/* <Route path="/table" element={<Table />} /> */}
-        <Route path="/twenty" element={<Twenty />} />
-        <Route path="/tr" element={<TriggerRendererProp />} />
+        <Route path="/smslog" element={<SmsLog />} />
       </Routes>
     </div>
   );
